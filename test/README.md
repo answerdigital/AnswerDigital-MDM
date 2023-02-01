2f104a6e5b37e91fdf825960332b95abe8ddfd8d
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-02-01 11:16:48 +0000

    Parameterized the Test and added a README

---
+++ test/README.md
@@ -0,0 +1,17 @@
+# Installation Script
+
+Simple Framework to validate a basic installation of an MDM application
+
+`pip install -r requirements.txt`
+
+You will need the required playwright binaries in order to run the login script.
+
+`playwright install`
+
+Then to execute the tests
+
+`pytest test.py`
+
+And to run the test with a visible browser
+
+`pytest --headed test.py`
--- test/test.py
+++ test/test.py
@@ -5,10 +5,13 @@
 from playwright.sync_api import Page, expect
 import requests
 import json
+import os
+
+url = os.getenv('MDM_INSTANCE_BASE_URL', 'http://localhost:8082/')
 
 
 def test_login(page: Page):
-    page.goto("http://localhost:8082/")
+    page.goto(url)
     page.locator("#mdm--navbar-desktop").get_by_role("button", name="Log in").click()
     page.get_by_placeholder("Enter your email").click()
     page.get_by_placeholder("Enter your email").fill("admin@maurodatamapper.com")
@@ -19,7 +22,7 @@ def test_login(page: Page):
 
 def test_check_configuration(snapshot):
     s = requests.Session()
-    url = "http://localhost:8082/api/authentication/login"
+    endpoint = f"{url}api/authentication/login"
     payload = json.dumps({
         "username": "admin@maurodatamapper.com",
         "password": "password"
@@ -27,11 +30,10 @@ def test_check_configuration(snapshot):
     headers = {
         'Content-Type': 'application/json'
     }
-    s.request("POST", url, headers=headers, data=payload)
-    url = "http://localhost:8082/api/admin/modules"
-    payload = {}
-    response = s.request("GET", url, headers=headers, data=payload)
+    s.request("POST", endpoint, headers=headers, data=payload)
+    endpoint = f"{url}api/admin/modules"
+    response = s.request("GET", endpoint, headers=headers, data={})
     snapshot.assert_match(response.text, 'plugin_response')
-    url = "http://localhost:8082/api/admin/properties"
-    response = s.request("GET", url, headers=headers, data=payload)
+    endpoint = f"{url}api/admin/properties"
+    response = s.request("GET", endpoint, headers=headers, data={})
     snapshot.assert_match(response.text, 'properties_response')
