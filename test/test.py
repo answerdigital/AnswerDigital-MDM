61724e7ba98de8cc9034e471552dddc0ce0bb610
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-02-01 11:26:51 +0000

    Ignore venv in parent directory and remove unused var

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 import requests
 import json
 import os
