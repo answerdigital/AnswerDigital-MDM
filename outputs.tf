3c78dcc0673e66dbb101395ef80e93dab68fac36
AudriusPutys <audrius.putys@answerdigital.com>
2023-02-13 11:23:37 +0000

    MDM-25 secondary Db endpoint added to output

--- outputs.tf
+++ outputs.tf
@@ -15,6 +15,11 @@ output "database_endpoint" {
   value       = aws_rds_cluster_instance.postgres_primary_instance[0].endpoint
 }
 
+output "secondary_database_endpoint" {
+  description = "The endpoint of the database"
+  value       = aws_rds_cluster_instance.postgres_secondary_instance[0].endpoint
+}
+
 output "database_port" {
   description = "The port of the database"
   value       = aws_rds_cluster.postgres_cluster.port
