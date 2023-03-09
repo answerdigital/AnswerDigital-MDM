74a7d46ea9325105802e802ad311b6edf314de86
Oleksii <oleksi.rybak@gmail.com>
2023-03-09 15:53:02 +0000

    removing unused variables from prod env

--- prod/variables.tf
+++ prod/variables.tf
@@ -37,65 +37,12 @@ variable "http_protocol" {
   default = "HTTP"
 }
 
-variable "https_protocol" {
-  default = "HTTPS"
-}
-
 variable "ssh_server_port" {
   default = 22
 }
+
 variable "container_internal_port" {
   default = 8080
-}
-
-variable "ec2_key_name" {
-  description = "SSH Key"
-  type        = string
-  default     = "MyAwsKey"
-
-}
-
-variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_count" {
-  description = "Number of subnets"
-  type        = map(number)
-  default = {
-    public  = 1,
-    private = 2
-  }
-}
-
-variable "public_subnet_cidr_blocks" {
-  description = "Available CIDR blocks for public subnets"
-  type        = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-    "10.0.4.0/24",
-  ]
-}
-
-variable "private_subnet_cidr_blocks" {
-  description = "Available CIDR blocks for private subnets"
-  type        = list(string)
-  default = [
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
-    "10.0.104.0/24",
-  ]
-}
-
-variable "ssh_key" {
-  description = "existing ssh key"
-  type        = string
-  default     = "mdm-dev"
 }
 
 variable "az_west_a" {
@@ -146,13 +93,6 @@ variable "network_protocol" {
   type        = string
   description = "network protocol type"
   default     = "tcp"
-}
-
-
-variable "mdm_plugins_dev-test" {
-  type  = string
-  description = "Plugins required in Dev and Test environment"
-  default = "uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-artdecor:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-awsglue:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-csv:4.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-digital-object-identifiers:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-excel:5.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-fhir:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-database-sqlserver:8.1.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-database-mysql:7.1.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-database-oracle:6.1.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-database-postgresql:7.1.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-xsd:1.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-profile-dcat:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-profile-schema-org:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-authentication-openid-connect:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-freemarker:2.2.0;uk.ac.ox.softeng.maurodatamapper.plugins:mdm-plugin-sparql:2.2.0"
 }
 
 variable "mdm_plugins_prod" {
